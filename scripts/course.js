f792eaaa81dcbaf9a4e03c2394dede860b358241
ayusograce <adriayusof@gmail.com>
2025-03-25 21:18:09 -0500

    modal excercise

--- scripts/course.js
+++ scripts/course.js
@@ -120,6 +120,30 @@ function createCourse(filteredCourses){
 
         card.appendChild(name);
         document.querySelector(".container").appendChild(card);
+
+        card.addEventListener("click", () =>{
+            displayCourseDetails(course);
+        });
     });
 }
 
+const CourseDetails = document.querySelector("#course-details");
+
+// ESTO ES PARA EL MODAL QUE APARECE CUANDO SE CLICKEAN LOS CURSOS
+function displayCourseDetails(course){
+    CourseDetails.innerHTML = "";
+    CourseDetails.innerHTML = `
+        <button id="closeModal">X</button>
+        <h2>${course.subject} ${course.number}</h2>
+        <h3>${course.title}</h3>
+        <p><strong>Credits</strong>: ${course.credits}</p>
+        <p><strong>Certificate</strong>: ${course.certificate}</p>
+        <p>${course.description}</p>
+        <p><strong>Technologies</strong>: ${course.technology}</p>
+    `;
+    CourseDetails.showModal();
+
+    closeModal.addEventListener("click", () => {
+        CourseDetails.close();
+    });
+}
