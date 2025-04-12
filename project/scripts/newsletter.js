232f813b42dffc70ce78876d5c9570ebe9d4880e
ayusograce <adriayusof@gmail.com>
2025-04-12 02:24:24 -0500

    home page completed

---
+++ project/scripts/newsletter.js
@@ -0,0 +1,18 @@
+
+
+
+let boton = document.querySelector("#subscribe");
+let form = document.querySelector("form");
+let close = document.querySelector("#closeModal");
+let modal = document.querySelector("#thank-you");
+
+
+form.addEventListener("submit", (x) =>{
+    x.preventDefault();
+    modal.showModal();
+});
+
+
+closeModal.addEventListener("click", () => {
+    modal.close();
+})
---
+++ project/scripts/quotes.js
@@ -0,0 +1,48 @@
+// GET A RANDOM QUOTE FROM DE QUOTES DATA IN THE JSON FILE.
+
+
+const datos = './data/quotes.json';
+const quotesCard = document.querySelector(".quote");
+
+async function getQuotesData(){
+    try{
+        const response = await fetch(datos);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            createQuotesCards(data);
+        } else{
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+function createQuotesCards(datos){
+    quotesCard.innerHTML = "";
+
+    let citas = datos.quotes || [];
+
+    let randomQuote = getRandom(citas)[0];
+
+    let card = document.createElement("div");
+    let actualQuote = document.createElement("p");
+    let authorBook = document.createElement("p");
+
+    actualQuote.textContent = randomQuote.quote;
+    authorBook.setAttribute("class", 'author');
+    authorBook.textContent = `-${randomQuote.author} (${randomQuote.book})`;
+
+    card.appendChild(actualQuote);
+    card.appendChild(authorBook);
+
+    quotesCard.appendChild(card);
+}
+
+function getRandom(array){
+    let random = array.sort(() => 0.5 - Math.random());
+    return random.slice(0, 1);
+}
+
+getQuotesData();
